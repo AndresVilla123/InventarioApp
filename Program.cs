cfbd74e3f8836b835e735d7a99c9f405890cd7e0
Andres Felipe Correa Villa <andres.correa@cadena.com.co>
2026-03-28 17:12:49 -0500

    firts push based structure

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine($"Assembly version: {version}");
