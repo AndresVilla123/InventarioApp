86bc532192343bd0570f48a1d5174de18c436690
Andres Felipe Correa Villa <andres.correa@cadena.com.co>
2026-03-28 17:29:28 -0500

    new src folder with models folder

--- Program.cs
+++ Program.cs
@@ -7,3 +7,13 @@ var assembly = Assembly.GetExecutingAssembly();
 var version = assembly.GetName().Version;
 
 Console.WriteLine($"Assembly version: {version}");
+
+// Progress project
+
+Console.WriteLine("Project structure");
+Console.WriteLine("-----------------");
+
+Console.WriteLine("InventarioApp.csjproj");
+Console.WriteLine("Program.cs");
+Console.WriteLine("README.md");
+Console.WriteLine("sr/Models");
